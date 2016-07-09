62f8d72c82c6492617e844503b881c5642f72bb7
Shunsuke <os18700241@gmail.com>
2016-07-09 16:22:45 +0900

    登録フォーム完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,16 +25,14 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        redirect_to regist_complete_path
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
+  end
+
+  def complete
   end
 
   # PATCH/PUT /users/1
