ea183e18dc8aeabf7f53d596d1b7159a43552f9f
Shunsuke <os18700241@gmail.com>
2016-07-13 02:48:45 +0900

    英語切り替え機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
-  get 'regist_complete' => 'users#complete'
-  root 'users#new'
 
+  scope "(:locale)", locale: /ja|en/  do
+    resources :users
+    get 'regist_complete' => 'users#complete'
+    root 'users#new'
+  end
 
 end
